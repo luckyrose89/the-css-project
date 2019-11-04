442eae5124d421d982645b0f33d71e79598c7d01
avidrutham <divyamathur89@gmail.com>
2019-11-04 16:00:13 +0530

    add js for modal

---
+++ shared.js
@@ -0,0 +1,19 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+
+function closeModal() {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+}
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function() {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+backdrop.addEventListener("click", closeModal);
+modalNoButton.addEventListener("click", closeModal);
