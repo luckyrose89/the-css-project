4200a951df858fb733c12d13c9cb294391e2165b
avidrutham <divyamathur89@gmail.com>
2019-11-05 12:16:39 +0530

    add mobile nav

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 function closeModal() {
   modal.style.display = "none";
@@ -15,5 +17,13 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
   });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function() {
+  mobileNav.style.display = "none";
+  closeModal();
+});
 modalNoButton.addEventListener("click", closeModal);
+
+toggleButton.addEventListener("click", function() {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
